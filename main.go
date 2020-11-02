fff7dd5e615050991ac7becdf79d503c8b7ed065
marianomon <monterosmarianos@hotmail.com.ar>
2020-11-01 23:56:59 -0300

    problema slice object

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type auto struct {
+	nombre string
+	modelo string
+}
+
+type agencia struct {
+	autos := make([]auto, 3)
+}
+
+func (a *auto) print() {
+	for {
+		fmt.Println(autos)
+	}
+}
+
+func (a agencia) addauto(c auto){
+	autos = append(inlineSlice, c)
+}
+
+func main() {
+	a := &auto{nombre: "BMW", modelo: "135i"}
+	a.print()
+}
